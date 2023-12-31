5ab2e67690222d3daf0ae8586acd3f29d44ee760
M0enster <dennisdmczx@gmail.com>
2023-12-31 20:02:08 +0100

    React interactivity: Events and state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,24 @@
-function Form() {
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+function Form(props) {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -12,6 +30,8 @@ function Form() {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
@@ -19,5 +39,9 @@ function Form() {
     </form>
   );
 }
+
+Form.propTypes = {
+  addTask: PropTypes.func.isRequired,
+};
 
 export default Form;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,12 @@ function Todo(props) {
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
@@ -12,7 +17,11 @@ function Todo(props) {
         <button type="button" className="btn">
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
-        <button type="button" className="btn btn__danger">
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
@@ -24,6 +33,8 @@ Todo.propTypes = {
   name: PropTypes.string.isRequired,
   completed: PropTypes.bool,
   id: PropTypes.string.isRequired,
+  toggleTaskCompleted: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Todo;
